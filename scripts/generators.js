c71aecd6b12c1c7a778ebd4e3705faf63152c403
Luis <81855420+luisoos@users.noreply.github.com>
2022-08-11 21:13:31 +0200

    Update generators.js
    Fixing uploadhost variable

--- scripts/generators.js
+++ scripts/generators.js
@@ -14,37 +14,37 @@ function randomString(length) {
 
 function generatePHP(key) {
   const content =   `<?php ini_set('display_errors', 1);
-                    ini_set('display_startup_errors', 1);
-                    error_reporting(E_ALL);
-                    header("Content-Type: text/text");
-                    $str = file_get_contents ('./config.json');
-                    $json = json_decode($str, true);
-                    $key = "${key}";
-                    $uploadhost = "${domain}";
-                    $redirect = "https://${domain}";
-                    if ($_SERVER["REQUEST_URI"] == "/robot.txt") {
-                        die("User-agent: *\\nDisallow: /");
-                    }
-                    if (isset($_POST['k'])) {
-                        if ($_POST['k'] == $key) {
-                            $target = getcwd() . "/" . basename($_FILES['d']['name']);
-                            if (move_uploaded_file($_FILES['d']['tmp_name'], $target)) {
-                                $md5 = md5_file(getcwd() . "/" . basename($_FILES['d']['name']));
-                                $explode = explode(".", $_FILES["d"]["name"]);
-                                rename(getcwd() . "/" . basename($_FILES['d']['name']), "/var/www/${domain}/upload/end/" . $_POST['n'] . "." . $md5 . "." . end($explode));
-                                if ($json['embed']) {
-                                echo $uploadhost . $_POST['n'] . '/' . $md5 . "/" . $md5 . '.html';
+                        ini_set('display_startup_errors', 1);
+                        error_reporting(E_ALL);
+                        header("Content-Type: text/text");
+                        $str = file_get_contents ('./config.json');
+                        $json = json_decode($str, true);
+                        $key = "${key}";
+                        $uploadhost = "https://${domain}/";
+                        $redirect = "https://${domain}";
+                        if ($_SERVER["REQUEST_URI"] == "/robot.txt") {
+                            die("User-agent: *\\nDisallow: /");
+                        }
+                        if (isset($_POST['k'])) {
+                            if ($_POST['k'] == $key) {
+                                $target = getcwd() . "/" . basename($_FILES['d']['name']);
+                                if (move_uploaded_file($_FILES['d']['tmp_name'], $target)) {
+                                    $md5 = md5_file(getcwd() . "/" . basename($_FILES['d']['name']));
+                                    $explode = explode(".", $_FILES["d"]["name"]);
+                                    rename(getcwd() . "/" . basename($_FILES['d']['name']), "/var/www/${domain}/upload/end/" . $_POST['n'] . "." . $md5 . "." . end($explode));
+                                    if ($json['embed']) {
+                                        echo $uploadhost . $_POST['n'] . '/' . $md5 . "/" . $md5 . '.html';
+                                    } else {
+                                        echo $uploadhost . $_POST['n'] . '/' . $md5 . "/" . $md5 . '.png';
+                                    }
+                                } else {
+                                    echo "Your file could not be uploaded!";
+                                }
                             } else {
-                                echo $uploadhost . $_POST['n'] . '/' . $md5 . "/" . $md5 . '.png';
-                            }
-                            } else {
-                                echo "Your file could not be uploaded! :(";
+                                header('Location: ' . $redirect);
                             }
                         } else {
                             header('Location: ' . $redirect);
-                        }
-                    } else {
-                        header('Location: ' . $redirect);
                     } ?>`;
 
     return content;
